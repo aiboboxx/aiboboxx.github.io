dde2fa41dc930e289fd4359b008994f077963641
aiboboxx <aiboboxx@gmail.com>
2022-02-13 17:11:25 +0800

    update from gridea: 2022-02-13 17:11:25

--- f/js/port.js
+++ f/js/port.js
@@ -1,3 +1,5 @@
 var port=8003;
-var ajaxUrl = `https://app.aiboboxx.ml:${port}/getrss.php`;
-var ajaxUrl_vf = `https://app.aiboboxx.ml:${port}/getrss_a.php`;
+//var ajaxUrl = `https://app.aiboboxx.ml:${port}/getrss.php`;
+//var ajaxUrl_vf = `https://app.aiboboxx.ml:${port}/getrss_a.php`;
+var ajaxUrl = `https://appls.eu.org//getrss.php`;
+var ajaxUrl_vf = `https://appls.eu.org//getrss_a.php`;
