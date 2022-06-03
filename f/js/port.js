58001e45cd6a35dacf8a17b8886e7ce42e9b426a
aiboboxx <aiboboxx@gmail.com>
2022-06-03 22:29:55 +0800

    update from gridea: 2022-06-03 22:29:55

--- f/js/port.js
+++ f/js/port.js
@@ -1,7 +1,7 @@
 var port=8003;
-//var ajaxUrl = `https://app.aiboboxx.ml:${port}/getrss.php`;
-//var ajaxUrlClash = `https://app.aiboboxx.ml:${port}/getclash.php`;
-//var ajaxUrl_vf = `https://app.aiboboxx.ml:${port}/getrss_vf.php`;
+// var ajaxUrl = `https://app.aiboboxx.ml:${port}/getrss.php`;
+// var ajaxUrlClash = `https://app.aiboboxx.ml:${port}/getclash.php`;
+// var ajaxUrl_vf = `https://app.aiboboxx.ml:${port}/getrss_vf.php`;
 var ajaxUrl = `https://appls.eu.org/getrss.php`;
 var ajaxUrlClash = `https://appls.eu.org/getclash.php`;
 var ajaxUrl_vf = `https://appls.eu.org/getrss_vf.php`;
